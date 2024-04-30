d5e1055457391903b63db920de875bc526522016
Prakhar Prasad <prakhar3009@gmail.com>
2024-04-30 12:58:13 +0530

    fixes

--- public/js/work.js
+++ public/js/work.js
@@ -1,5 +1,5 @@
-var ticker = document.querySelector(".ticker-2"),
-  list = document.querySelector(".ticker-list-2"),
+var ticker = document.querySelector(".ticker-4"),
+  list = document.querySelector(".ticker-list-4"),
   clone = list.cloneNode(true);
 
 ticker.append(clone);
